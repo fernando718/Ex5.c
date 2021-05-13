b020cda99137b43ac446337c1f81898da4baea1b
fernando718 <82851141+fernando718@users.noreply.github.com>
2021-05-12 21:12:14 -0500

    Create Ex5.c

---
+++ Ex5.c
@@ -0,0 +1,14 @@
+//VALDIVIA GONZALEZ JOSE FERNANDO// //4IV17//
+#include >stdio.h> 
+int main ()
+[
+   float numerodepersonas, numerodealimentos, porciones;
+   printf ("\n\n Programa 5");
+   printf ("\n\n Dame el numero de personas");
+   scanf ("%f",&numerodepersonas); 
+   printf ("\n\n Dame el numero de alimentos ");
+   scanf ("%f",&numerodealimentos); 
+   porciones para cada persona= numerodepersonas/numerodealimentos;
+   printf  ("\n\n la porcion para dividir equitativamente a cade persona es  &f");
+   return 0;
+] 
